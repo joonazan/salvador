036247b95fd6a0a4a557eca03a7490e84fc65f9b
Joonatan Saarhelo <joon.saar@gmail.com>
2016-08-17 18:56:22 +0300

    dali commands for addressing

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const address = (hasLEQ, onFind) => {
+import parallel from 'async/parallel'
+
+const genericAddressing = (hasLEQ, onFind) => {
 	const MAX = 16777216
 	var start = 0
 	var end = MAX
@@ -37,6 +39,29 @@ const selectMiddle = (start, end) => {
 	return Math.floor((start + end) / 2)
 }
 
+const address = dali => {
+	var smallestFreeShortAddress = 0
+	genericAddressing(
+		(x, cb) => {
+			broadcastLongAddress(x)
+			dali(0xA9, 0, resp => {cb(resp == 0xFF)})
+		},
+		(x, cb) => {
+			const a = smallestFreeShortAddress
+			smallestFreeShortAddress++
+			broadcastLongAddress(x)
+			dali(0xB7, a, cb)
+		}
+	)
+}
+
+const broadcastLongAddress = (dali, x) => {
+	dali(0xB1, x >> 16)
+	dali(0xB3, (x >> 8) & 0xFF)
+	dali(0xB5, x & 0xFF)
+}
+
 module.exports = {
+	genericAddressing,
 	address
 }
